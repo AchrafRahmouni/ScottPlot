fdfb12e688b8cab83b0360530cd1493e4289eab3
Scott W Harden <swharden@gmail.com>
2018-01-23 19:13:29 -0500

    its starting!

---
+++ src/ScottPlotV2/ScottPlotV2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // form click bindings
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/ScottPlotV2/ScottPlotV2/ScottPlot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotV2
+{
+    class ScottPlot
+    {
+
+        private int figure_width, figure_height;
+
+        /// <summary>
+        /// Create a new ScottPlot
+        /// </summary>
+        /// <param name="figure_width">size of the image (pixels)</param>
+        /// <param name="figure_height">size of the image (pixels)</param>
+        public ScottPlot(int figure_width=640, int figure_height=480)
+        {
+            this.figure_width = figure_width;
+            this.figure_height = figure_height;
+        }
+    }
+}
